f75e5fe3db4c79d4de70c926a58e127776ffd5d4
1Vasiliy <cagefighter565@gmail.com>
2019-10-15 22:54:48 +0700

    Add fxml file and Controller class

---
+++ src/main/java/game/Main.java
@@ -0,0 +1,10 @@
+package game;
+
+import static javafx.application.Application.launch;
+
+public class Main {
+
+    public static void main(String[] args) {
+        launch(Game.class, args);
+    }
+}
